bebfe8114499c62cae6867df9c8156c4673ca01d
Michael Brodhead <mkb@engineyard.com>
2012-07-26 21:32:40 -0700

    oops.  that was stupid.  now not requiring awesome_print either.  #TDD

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 $: << './lib'
 require 'riak-shim'
 require 'pry'
-require 'awesome_print'
 
 ENV['RACK_ENV'] = 'test' unless ENV['RACK_ENV']
 DB_CONFIG = { 'development' => { 'bucket_prefix' => 'dev_', 'host' => "localhost", 'http_port' => 8098},
