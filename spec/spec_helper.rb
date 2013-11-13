d2891a0e080341f440761319d0918c8ea98fb6e8
Michael Brodhead <mkb@orthogonal.org>
2013-11-12 16:25:42 -0800

    Fuck RubyGems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 $: << './lib'
 require 'riak-shim'
-require 'pry' if Gem.available?('pry')
 require 'awesome_print'
+
+begin
+  require 'pry'
+rescue LoadError
+end
 
 ENV['RACK_ENV'] = 'test' unless ENV['RACK_ENV']
 DB_CONFIG = { 'development' => { 'bucket_prefix' => 'dev_', 'host' => "localhost", 'http_port' => 8098},
